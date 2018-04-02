c8bc4daf91b534725e7b4916ff4c1be38d77b16d
Danguy Des Deserts Venceslas <wenceslas.desdeserts@laposte.net>
2018-04-02 23:08:32 +0200

    Revert to python 3.5

--- Model.py
+++ Model.py
@@ -13,22 +13,22 @@ def encoder(e_input):
     leaky_index = count(start=1)
     add_index = count(start=1)
 
-    e = Conv2D(filters=64, kernel_size=(5, 5), padding='valid', strides=(2, 2), name=f"e_conv_{next(conv_index)}")(e_input)
-    e = LeakyReLU(alpha=a, name=f"e_leaky_{next(leaky_index)}")(e)
-    e = Conv2D(filters=128, kernel_size=(5, 5), padding='valid', strides=(2, 2), name=f"e_conv_{next(conv_index)}")(e)
-    e = LeakyReLU(alpha=a, name=f"e_leaky_{next(leaky_index)}")(e)
+    e = Conv2D(filters=64, kernel_size=(5, 5), padding='valid', strides=(2, 2), name="e_conv_" + str(str(next(conv_index))))(e_input)
+    e = LeakyReLU(alpha=a, name="e_leaky_" + str(next(leaky_index)))(e)
+    e = Conv2D(filters=128, kernel_size=(5, 5), padding='valid', strides=(2, 2), name="e_conv_" + str(next(conv_index)))(e)
+    e = LeakyReLU(alpha=a, name="e_leaky_" + str(next(leaky_index)))(e)
 
     e_skip_connection = e
 
     # Create three residual blocks
     for i in range(3):
-        e = Conv2D(name=f"e_conv_{next(conv_index)}", **e_res_block_conv_params)(e)
-        e = LeakyReLU(alpha=a, name=f"e_leaky_{next(leaky_index)}")(e)
-        e = Conv2D(name=f"e_conv_{next(conv_index)}", **e_res_block_conv_params)(e)
-        e = Add(name=f"e_add_{next(add_index)}")([e, e_skip_connection])
+        e = Conv2D(name="e_conv_" + str(next(conv_index)), **e_res_block_conv_params)(e)
+        e = LeakyReLU(alpha=a, name="e_leaky_" + str(next(leaky_index)))(e)
+        e = Conv2D(name="e_conv_" + str(next(conv_index)), **e_res_block_conv_params)(e)
+        e = Add(name="e_add_" + str(next(add_index)))([e, e_skip_connection])
         e_skip_connection = e
 
-    e = Conv2D(filters=96, kernel_size=(5, 5), padding='valid', strides=(2, 2), name=f"e_conv_{next(conv_index)}")(e)
+    e = Conv2D(filters=96, kernel_size=(5, 5), padding='valid', strides=(2, 2), name="e_conv_" + str(next(conv_index)))(e)
     encoded = RoundingLayer()(e)
 
     return encoded
@@ -40,24 +40,24 @@ def decoder(encoded,d_mean,d_std):
     leaky_index = count(start=1)
     add_index = count(start=1)
 
-    d = Conv2D(filters=512, kernel_size=(3, 3), padding='same', strides=(1, 1), name=f"d_conv_{next(conv_index)}")(encoded)
-    d = Lambda(function=subpixel, name=f"d_lambda_{next(lambda_index)}")(d)
+    d = Conv2D(filters=512, kernel_size=(3, 3), padding='same', strides=(1, 1), name="d_conv_" + str(next(conv_index)))(encoded)
+    d = Lambda(function=subpixel, name="d_lambda_" + str(next(lambda_index)))(d)
 
     d_skip_connection = d
 
     # Add three residual blocks
     for j in range(3):
-        d = Conv2D(name=f"d_conv_{next(conv_index)}", **d_res_block_conv_params)(d)
-        d = LeakyReLU(alpha=a, name=f"d_leaky_{next(leaky_index)}")(d)
-        d = Conv2D(name=f"d_conv_{next(conv_index)}", **d_res_block_conv_params)(d)
-        d = Add(name=f"d_add_{next(add_index)}")([d, d_skip_connection])
+        d = Conv2D(name="d_conv_" + str(next(conv_index)), **d_res_block_conv_params)(d)
+        d = LeakyReLU(alpha=a, name="d_leaky_" + str(next(leaky_index)))(d)
+        d = Conv2D(name="d_conv_" + str(next(conv_index)), **d_res_block_conv_params)(d)
+        d = Add(name="d_add_" + str(next(add_index)))([d, d_skip_connection])
         d_skip_connection = d
 
-    d = Conv2D(filters=256, kernel_size=(3, 3), padding='same', strides=(1, 1), name=f"d_conv_{next(conv_index)}")(d)
-    d = Lambda(function=subpixel, name=f"d_lambda_{next(lambda_index)}")(d)
+    d = Conv2D(filters=256, kernel_size=(3, 3), padding='same', strides=(1, 1), name="d_conv_" + str(next(conv_index)))(d)
+    d = Lambda(function=subpixel, name="d_lambda_" + str(next(lambda_index)))(d)
 
-    d = Conv2D(filters=12, kernel_size=(3, 3), padding='same', strides=(1, 1), name=f"d_conv_{next(conv_index)}")(d)
-    d = Lambda(function=subpixel, name=f"d_lambda_{next(lambda_index)}")(d)
+    d = Conv2D(filters=12, kernel_size=(3, 3), padding='same', strides=(1, 1), name="d_conv_" + str(next(conv_index)))(d)
+    d = Lambda(function=subpixel, name="d_lambda_" + str(next(lambda_index)))(d)
 
     # Denormalize
     d = Multiply()([d,d_std])
