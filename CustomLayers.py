fixing normalisation/denormalisation/mirror padding adding preprocessing

CustomLayers.py
@@ -66,22 +66,64 @@ class RoundingLayer(Layer):
     def compute_output_shape(self, input_shape):
         return input_shape
     
-class MaskingLayer(Layer):
+class MeanLayer(Layer):
     def __init__(self, **kwargs):
-        super(MaskingLayer, self).__init__()
+        super(MeanLayer, self).__init__()
         self.supports_masking = False
-        self.mask_idx = kwargs["mask_idx"]
 
     def build(self, input_shape):
         self.trainable_weights = []
-        self.mask = np.zeros(input_shape[1:])
-        for i in range(self.mask_idx):
-            self.mask[i%input_shape[1],i//input_shape[1]%input_shape[2],:] = 1.0
-
-        super(MaskingLayer, self).build(input_shape)
+        super(MeanLayer, self).build(input_shape)
 
     def call(self, x, mask=None):
-        return tf.multiply(x,self.mask)
+        return tf.reshape(tf.reduce_mean(x,axis = (1,2)),(-1,1,1,3))
 
     def compute_output_shape(self, input_shape):
         return input_shape
+
+class StdLayer(Layer):
+    def __init__(self, **kwargs):
+        super(StdLayer, self).__init__()
+        self.supports_masking = False
+
+    def build(self, input_shape):
+        self.trainable_weights = []
+        super(StdLayer, self).build(input_shape)
+
+    def call(self, x, mask=None):
+        return tf.reshape(tf.keras.backend.std(x,axis = (1,2)),(-1,1,1,3))
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+class MirrorPaddingLayer(Layer):
+    def __init__(self, **kwargs):
+        super(MirrorPaddingLayer, self).__init__()
+        self.supports_masking = False
+
+    def build(self, input_shape):
+        self.trainable_weights = []
+        super(MirrorPaddingLayer, self).build(input_shape)
+
+    def call(self, x, mask=None):
+        paddings = tf.constant([[0,0],[12,12],[12,12],[0,0]])
+        return tf.pad(x, paddings = paddings, mode = "REFLECT")
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+class MultiplyLayer(Layer):
+    def __init__(self, **kwargs):
+        super(MultiplyLayer, self).__init__()
+        self.supports_masking = False
+
+    def build(self, input_shape):
+        self.trainable_weights = []
+        super(MultiplyLayer, self).build(input_shape)
+
+    def call(self, x, mask=None):
+        return tf.multiply(x[0],x[1])
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+

Generator.py
@@ -15,8 +15,14 @@ class DataGenerator(keras.utils.Sequence):
         self.folder = folder
         self.len_data = len(img_list)
         self.shuffle = shuffle
-        self.on_epoch_end()
+
         self.mask_idx = 2
+        self.mask = np.zeros((self.batch_size,img_input_shape[0]//8,img_input_shape[1]//8,96))
+        for i in range(self.mask_idx):
+            self.mask[:,i%self.mask.shape[1],i//self.mask.shape[1]%self.mask.shape[2],:] = 1.0
+        self.update_mask = False
+        self.on_epoch_end()
+
 
     def __len__(self):
         'Denotes the number of batches per epoch'
@@ -31,18 +37,20 @@ class DataGenerator(keras.utils.Sequence):
         img_temp = [self.img_list[k] for k in indexes]
 
         # Generate data
-        X_padded, X_mean, X_std, X = self.__data_generation(img_temp)
-
-        mask = np.zeros((self.batch_size,img_input_shape[0]//8,img_input_shape[1]//8,96))
-        print("generating mask with {} 1".format(self.mask_idx))
-        for i in range(self.mask_idx):
-            mask[:,i%mask.shape[1],i//mask.shape[1]%mask.shape[2],:] = 1.0
-
-        return [X_padded, X_mean ,X_std, mask], X
+        X = self.__data_generation(img_temp)
+        return [X, self.mask], X
 
     def on_epoch_end(self):
         'Updates indexes after each epoch'
         self.indexes = np.arange(self.len_data)
+        
+
+        if self.update_mask:
+            print("generating mask with {} 1".format(self.mask_idx))
+            for i in range(self.mask_idx):
+                self.mask[:,i%self.mask.shape[1],i//self.mask.shape[1]%self.mask.shape[2],:] = 1.0
+            self.update_mask = False
+        
         if self.shuffle == True:
             np.random.shuffle(self.indexes)
 
@@ -51,21 +59,11 @@ class DataGenerator(keras.utils.Sequence):
         # Initialization
         X = np.empty((self.batch_size, *self.dim))
 
-        # compute the dimension after padding
-        padded_shape = (self.dim[0]+2*mirror,self.dim[1]+2*mirror,self.dim[2])
-        X_padded = np.empty((self.batch_size, *padded_shape ))
-        X_mean = np.empty((self.batch_size,3))
-        X_std = np.empty((self.batch_size,3))
-
         # Generate data
         for i in range(len(img_temp)):
             # Store sample
             img = PIL.Image.open(self.folder + "/" + img_temp[i])
-            img = img.resize(img_input_shape[0:2], PIL.Image.ANTIALIAS)
-            X[i,] = img
-            X_mean[i,] = np.mean(X[i,],axis = (0,1))
-            X_std[i,] = np.std(X[i,], axis = (0,1))
-            X[i,] = (X[i,]-X_mean[i,])/X_std[i,]
-            X_padded[i,] = mirror_padding(X[i,],mirror)
+            X[i,] = np.asarray(img)
 
-        return X_padded, X_mean, X_std, X
+
+        return X

main.py
@@ -1,61 +1,69 @@
 import os
 import numpy as np
-from keras.utils import plot_model
-from keras.callbacks import TensorBoard, EarlyStopping
+#from keras.utils import plot_model
+from keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint
 from keras.datasets import cifar10
+from keras.optimizers import Adam
 from Model import build_model
 from ModelConfig import e_input_shape,img_input_shape
 from CustomLoss import loss
-from utils import mirror_padding
 from Generator import DataGenerator
-from CustomCallback import IncrementalMask
-
-import keras.backend as K
+import PIL
+import preprocessing
+""" import keras.backend as K
 from tensorflow.python import debug as tf_debug
 sess = K.get_session()
 sess = tf_debug.LocalCLIDebugWrapperSession(sess)
 K.set_session(sess)
-
-img_list = os.listdir("data")
-train_ratio = 0.7
-val_ratio = 0.2
-
-seed = np.random.seed(8)
-indices = np.arange(len(img_list))
-indices = np.random.permutation(indices)
-
-train_index = int(train_ratio*len(img_list))
-val_index = int(train_ratio*len(img_list))+int(val_ratio*len(img_list))
+ """
 
 
+img_train = os.listdir('working_data/train')
+img_val = os.listdir('working_data/val')
+img_test = os.listdir('working_data/test')
 
-img_train = [img_list[i] for i in indices[:train_index]]
-img_val = [img_list[i] for i in indices[train_index:val_index]]
-img_test = [img_list[i] for i in indices[val_index:]]
-
-train_generator = DataGenerator("data",img_train,7,img_input_shape)
-test_generator = DataGenerator("data",img_test,len(img_test),img_input_shape)
+train_generator = DataGenerator('working_data/train',img_train,32,img_input_shape)
+test_generator = DataGenerator('working_data/val',img_val,len(img_val),img_input_shape)
 
 
 autoencoder = build_model()
 
+
 print("encoding image into vector with shape {}".format(autoencoder.layers[18].output_shape))
 
 # Plot model graph
-plot_model(autoencoder, to_file='autoencoder.png')
+#plot_model(autoencoder, to_file='autoencoder.png')
 
 # Compile model with adadelta optimizer
 # TODO: Code loss !
-autoencoder.compile(optimizer='adadelta', loss=loss)
 
+# optimizer
+optimizer = Adam(lr = 1e-4)
 
+# compilation
+autoencoder.compile(optimizer=optimizer, loss=loss)
+checkpoint = ModelCheckpoint("weights.hdf5", save_best_only=True)
+
+iteration = 0
 # Train model !
 while train_generator.mask_idx < (img_input_shape[0]//8-1)**2:
     tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32)
-    early_stopping = EarlyStopping(monitor='val_loss', min_delta=1e-2, patience=5, verbose=0, mode='auto')
+    early_stopping = EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=20, verbose=0, mode='auto')
     autoencoder.fit_generator(train_generator,
                     epochs=100,
                     validation_data=(test_generator[0]),
-                    callbacks = [tensorboard,early_stopping])
+                    callbacks = [tensorboard,early_stopping, checkpoint])
+    
+
+    img_list = os.listdir("data")
+    img = preprocessing.read_image('data/' + img_list[0])
+
+    imgs = autoencoder.predict([img, np.ones((1,img_input_shape[0]//8,img_input_shape[1]//8,96))])
+
+    img = imgs[0,:,:,:]
+    img = PIL.Image.fromarray(np.uint8(img*255))
+    img.save("iteration_{}.png".format(iteration))
+
     train_generator.mask_idx += 1
     test_generator.mask_idx += 1
+    iteration += 1