5bba9b7b8411501373b8eebd6f65941fdec64361
MrMey <meynard.ro@gmail.com>
2018-04-03 15:46:11 +0200

    keeping Customcallbacks.py

--- CustomCallbacks.py
+++ CustomCallbacks.py
@@ -1,11 +1,15 @@
 from keras.callbacks import Callback
 import PIL
+import numpy as np
 
 class PredictCallback(Callback):
     def __init__(self,generator):
         self.generator = generator
 
-    def on_batch_end(self, batch, logs={}):
-        imgs = self.model.predict(self.generator[0])
-        img = PIL.Image.fromarray(numpy.uint8(imgs[0]))
-        img[0].show()
+    def on_epoch_end(self, batch, logs={}):
+        imgs = self.generator[0][0]
+        img = PIL.Image.fromarray(np.uint8(imgs[0]*255))
+        img.show()
+        imgs = self.model.predict(imgs)
+        img = PIL.Image.fromarray(np.uint8(imgs[0]*255))
+        img.show()
